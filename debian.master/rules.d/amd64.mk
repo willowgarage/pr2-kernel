fb7eab138bcf0c4ac5831aa0fc61899362ef83f2
willow <willow@scrooge.willowgarage.com>
2012-07-31 14:39:26 -0700

    Add debian package build files.

---
+++ debian.master/rules.d/amd64.mk
@@ -0,0 +1,10 @@
+human_arch	= 64 bit x86
+build_arch	= x86_64
+header_arch	= $(build_arch)
+defconfig	= defconfig
+flavours	= pr2 pr2-rt
+build_image	= bzImage
+kernel_file	= arch/$(build_arch)/boot/bzImage
+install_file	= vmlinuz
+loader		= grub
+no_dumpfile	= true
